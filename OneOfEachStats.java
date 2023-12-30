785b95d5b7b0a17ba73531f6c1b611f676c1b5dc
ShovalBiadgilin <Shoval.biadgilin@post.runi.ac.il>
2023-12-30 23:19:25 +0200

    Add awesome code for Homework 2

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -35,9 +35,9 @@ public class OneOfEachStats {
   }
 	   
 	  System.out.println("Average: " +(total/T)+ " children to get at least one of each gender.");
-      System.out.println(" Number of families with 2 children: " + count2);
-	  System.out.println(" Number of families with 3 children: " + count3);
-	  System.out.println(" Number of families with 4 or more children: " + count4);
+      System.out.println("Number of families with 2 children: " + count2);
+	  System.out.println("Number of families with 3 children: " + count3);
+	  System.out.println("Number of families with 4 or more children: " + count4);
 	  
 	  
 	      if (count2>count3 && count2>count4){ 
